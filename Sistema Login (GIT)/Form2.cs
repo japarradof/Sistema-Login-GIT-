f78ae2fbb3b8fcaf4100ef58cfd56a8ecbfe3380
Juan Antonio <152283035+TheHuanPa@users.noreply.github.com>
2024-12-18 12:11:13 +0100

    Interfaz1

--- Sistema Login (GIT)/Form2.cs
+++ Sistema Login (GIT)/Form2.cs
@@ -10,18 +10,16 @@ using System.Windows.Forms;
 
 namespace Sistema_Login__GIT_
 {
-    public partial class Form1 : Form
+    public partial class Form2 : Form
     {
-        public Form1()
+        public Form2()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void label1_Click(object sender, EventArgs e)
         {
-            Form2 form = new Form2();
-            form.ShowDialog();
-            this.Hide();
+
         }
     }
 }
